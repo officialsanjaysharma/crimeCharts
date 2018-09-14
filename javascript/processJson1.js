ee6f0a5e0cadea2b2a3eed8905a45bec4e333740
sanjay sharma <officialsanjaysharma@gmail.com>
2018-09-14 14:19:39 +0530

    FIles uploaded successfully

--- javascript/processJson1.js
+++ javascript/processJson1.js
@@ -12,6 +12,7 @@ const width = 960 - margin.left - margin.right;
 const height = 500 - margin.top - margin.bottom;
 const svg = d3.select('body')
   .append('svg')
+  .attr('id', 'x')
   .attr('width', width + margin.left + margin.right)
   .attr('height', height + margin.top + margin.bottom)
   .append('g')
